212bc1416e0480b71c9de5df336307ead0dd7e06
Matteo Borghesi <ma.borghe@gmail.com>
2020-11-11 10:01:15 +0100

    Improve utils

--- src/utils.py
+++ src/utils.py
@@ -1,25 +1,24 @@
 import numpy as np
 import keras
 import tensorflow as tf
+import data_setup
 from sklearn.metrics import accuracy_score
-
 
 # Hyperparameters
 batch_size = 32
-epochs = 20
 
 
-def train_model(model, x_train, y_train, x_val, y_val):
+def train_model(model, x_train, y_train, x_val, y_val, epochs):
     with tf.device("gpu:0"):
-        model.fit(x_train, y_train, batch_size=batch_size, epochs=30, verbose=1,
+        model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1,
                   validation_data=(x_val, y_val))
 
 
 # This function computes the accuracy and the uncertainty of a model wrt a set of samples
-def evaluate_model(model, x, y, num_classes, num_predictions):
+def evaluate_model(model, x, y, num_predictions):
     # predict real samples to compute 1. and 2.
     n_samples = x.shape[0]
-    p_hat = np.zeros((num_predictions, n_samples, num_classes))
+    p_hat = np.zeros((num_predictions, n_samples, data_setup.num_classes))
     for i in range(num_predictions):
         p_hat[i] = model.predict(x)
 
@@ -28,7 +27,7 @@ def evaluate_model(model, x, y, num_classes, num_predictions):
     y_pred = np.argmax(p_hat_avg,
                        axis=1)  # extract class with highest score for each sample, wich shape (n_samples,)
 
-    y_pred_one_hot = keras.utils.to_categorical(y_pred, num_classes)
+    y_pred_one_hot = keras.utils.to_categorical(y_pred, data_setup.num_classes)
     acc = accuracy_score(y, y_pred_one_hot)
     # Compute uncertainty
     epistemic = np.mean(p_hat ** 2, axis=1) - np.mean(p_hat, axis=1) ** 2
