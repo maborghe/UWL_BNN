dee6effd877228e85d3bce474af99cc4ba85adb2
Mattia Tarquinio <32510884+tarquo23@users.noreply.github.com>
2020-12-03 18:25:33 +0100

    Added all loss functions

--- src/loss.py
+++ src/loss.py
@@ -4,12 +4,73 @@ import keras
 
 # we define our custom loss function here
 # y_pred is the matrix of all T predictions for each sample
-def my_loss_fct(y_true, y_pred):
-    y_pred_mean, y_pred_uc = utils.compute_pred_distribution(y_pred)
+def my_loss_fct(y_true, y_pred):    
+  y_pred_mean, y_pred_uc = utils.compute_pred_distribution(y_pred)
+  pred_class = K.argmax(y_pred_mean, axis=-1)
+  pred_class = tf.reshape(pred_class, [-1, 1])
+  class_uc = tf.gather_nd(y_pred_uc, pred_class, batch_dims=1)  
+  
+  class_uc = 1 - class_uc
+  class_uc_inv = class_uc  
 
-    # TODO: now we have to arrays: y_pred_mean contains the class scores for each sample
-    # while y_pred_uc contains the uncertainty of that score.
-    # We have to take out a single scalar as the loss (e.g. we could compute the cross-entropy for
-    # each sample and then return a mean weighted by the uncertainty)
-    pred_loss = keras.losses.categorical_crossentropy(y_true, y_pred_mean)
-    return pred_loss
+  pred_loss = keras.losses.categorical_crossentropy(y_true, y_pred_mean)*class_uc_inv;  # we weight the loss by the computed weights
+  return pred_loss  
+
+
+def my_loss_fct2(y_true, y_pred):
+  
+  y_pred_mean, y_pred_uc = utils.compute_pred_distribution(y_pred)
+  pred_class = K.argmax(y_pred_mean, axis=-1)
+  pred_class = tf.reshape(pred_class, [-1, 1])
+  class_uc = tf.gather_nd(y_pred_uc, pred_class, batch_dims=1)  
+  
+  class_uc = K.pow(class_uc, 2)
+  class_uc = 1 - class_uc
+  class_uc_inv = class_uc  
+
+  pred_loss = keras.losses.categorical_crossentropy(y_true, y_pred_mean)*class_uc_inv;  # we weight the loss by the computed weights
+  return pred_loss
+
+def my_loss_fct3(y_true, y_pred):
+  
+  y_pred_mean, y_pred_uc = utils.compute_pred_distribution(y_pred)
+  pred_class = K.argmax(y_pred_mean, axis=-1)
+  pred_class = tf.reshape(pred_class, [-1, 1])
+  class_uc = tf.gather_nd(y_pred_uc, pred_class, batch_dims=1)  
+  
+  class_uc = K.pow(class_uc, 3)
+  class_uc = 1 - class_uc
+  class_uc_inv = class_uc  
+
+  pred_loss = keras.losses.categorical_crossentropy(y_true, y_pred_mean)*class_uc_inv;  # we weight the loss by the computed weights
+  return pred_loss  
+
+
+def my_loss_fct4(y_true, y_pred):
+    
+  y_pred_mean, y_pred_uc = utils.compute_pred_distribution(y_pred)
+  pred_class = K.argmax(y_pred_mean, axis=-1)
+  pred_class = tf.reshape(pred_class, [-1, 1])
+  class_uc = tf.gather_nd(y_pred_uc, pred_class, batch_dims=1)  
+  
+  class_uc /= keras.backend.mean(class_uc) 
+  class_uc = K.exp(class_uc)
+  class_uc_inv = 1 / class_uc  # in this way uncertain samples have a lower weight
+  class_uc_inv /= keras.backend.mean(class_uc_inv)  # we normalize the weights so that they average to 1
+
+  pred_loss = keras.losses.categorical_crossentropy(y_true, y_pred_mean)*class_uc_inv;  # we weight the loss by the computed weights  
+  return pred_loss  
+
+def my_loss_fct5(y_true, y_pred):
+
+  y_pred_mean, y_pred_uc = utils.compute_pred_distribution(y_pred)
+  pred_class = K.argmax(y_pred_mean, axis=-1)
+  pred_class = tf.reshape(pred_class, [-1, 1])
+  class_uc = tf.gather_nd(y_pred_uc, pred_class, batch_dims=1)  
+  
+  class_uc = K.exp(class_uc)
+  class_uc_inv = 1 / class_uc  # in this way uncertain samples have a lower weight
+  class_uc_inv /= keras.backend.mean(class_uc_inv)  # we normalize the weights so that they average to 1
+
+  pred_loss = keras.losses.categorical_crossentropy(y_true, y_pred_mean)*class_uc_inv;  # we weight the loss by the computed weights  
+  return pred_loss  
